d799373cb528a632d4fc7b86439f5094ae9383ea
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2024-01-06 13:58:02 -0330

    Hello World python function

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#Basic fucntion for printing hello world with specific name greeting
+def hello_world(name):
+    print("Hello world, my name is:", name)
+
+
+if __name__ == "__main__":
+    hello_world("Stephen")
+
